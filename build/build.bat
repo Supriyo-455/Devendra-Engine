b972105f1af8299f60870509241e9858ec9f502b
Supriyo Paul <paulsupriyo64nk@gmail.com>
2022-09-12 14:52:00 +0530

    Modern Opengl support

--- build/build.bat
+++ build/build.bat
@@ -1,2 +1,2 @@
 @echo off
-cl -FC -Zi ..\engine\src\Win64_Devendra.cpp OpenGL32.lib GLu32.lib Gdi32.lib User32.lib
+cl -FC -Zi ..\engine\Win64_Devendra.cpp OpenGL32.lib GLu32.lib Gdi32.lib User32.lib
